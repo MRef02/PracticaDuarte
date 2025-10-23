6a279b51ea3c7d78d84c5edbabd21e606a510ea0
MRef02 <marifercan73@gmail.com>
2025-10-23 08:50:27 -0600

    se creo metodos en la clase pruebas
    debido a que es necesario crear el esqueleto en la clase pruebas

---
+++ codigo1/clases/PruebasL.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace codigo1.clases;
+
+public class PruebasL
+{
+   
+    public void MetodoPruebaL()
+    {
+        Console.WriteLine("Método de prueba L ejecutado.");
+    }
+
+}
